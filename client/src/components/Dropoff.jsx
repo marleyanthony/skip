59fb13765bbb257750c5b7e463532f3f8de88111
marleyanthony <marleycanthony@gmail.com>
2020-09-04 07:54:31 -0700

    added json data

--- client/src/components/Dropoff.jsx
+++ client/src/components/Dropoff.jsx
@@ -4,6 +4,10 @@ function Dropoff() {
   return (
     <>
       <h1>Container Dropoff Page</h1>
+      <iframe
+
+        src="https://www.google.com/maps/embed/v1/place?key=API_KEY&q=Space+Needle,Seattle+WA" allowfullscreen>
+      </iframe>
     </>
   )
 }
