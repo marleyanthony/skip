a8f991f83350c9c8c04d33a5042cb88ce95827f1
Kamari Akers <kamari.akers@yahoo.com>
2020-09-04 16:14:20 -0400

    added final icons to footer component for social media companies

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import '../styles/partials/_footer.scss';
-import {Link} from 'react-router-dom';
+import Wave from '../assets/imgs/footer-wave.svg';
+import Facebook from '../assets/logos/facebook.svg';
+import Twitter from '../assets/logos/twitter.svg';
+import Instagram from '../assets/logos/instagram.svg';
 
 function Footer() {
   return (
     <>
+    <img src={Wave}></img>
     <footer className='footer'>
-      <ul className='footer__list'>
-          <li className='footer__list-item'>Go-Getters</li>
-          <li className='footer__list-item'>About Us</li>
-          <li className='footer__list-item'>Blog</li>
+        <ul className='footer__list'>
+            <li className='footer__list-item'>Go-Getters</li>
+            <li className='footer__list-item'>About Us</li>
+            <li className='footer__list-item'>Blog</li>
         </ul>
 
-      <div>
-        <Link to='https://www.facebook.com/'>
-          <img className='footer__logo'></img>
-        </Link>
-
-        <Link to='https://www.instagram.com/'>
-          <img className='footer__logo'></img>
-        </Link>
-      </div>
+        <div className='footer__logo-container'>
+            <img className='footer__logo' src={Facebook}></img>
+            <img className='footer__logo' src={Instagram}></img>
+            <img className='footer__logo' src={Twitter}></img>
+        </div>
     </footer>
     </>
   )
